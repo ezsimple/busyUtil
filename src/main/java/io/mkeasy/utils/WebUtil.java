4dea4ae829f1601a4a02d82b272a85eae92515bf
mhlee <mhlee@ezfarm.co.kr>
2020-09-10 15:33:43 +0900

    WebUtil.getResource 함수 추가

--- src/main/java/io/mkeasy/utils/WebUtil.java
+++ src/main/java/io/mkeasy/utils/WebUtil.java
@@ -2,9 +2,13 @@ package io.mkeasy.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletContext;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +18,7 @@ public class WebUtil {
 	/* Resource 는 static을 사용할 수 없습니다. */
 	@Resource 
 	AbstractApplicationContext ctx;
+
 
 	private String WEB_ROOT = null;
 	
@@ -27,6 +32,20 @@ public class WebUtil {
 		WEB_ROOT = f.getPath();
 
 		return WEB_ROOT;
+	}
+	
+	@Autowired
+	ServletContext context;
+
+	// 1. filePath 는 src/main/webapp 이하의 경로를 쓰면 됩니다.
+	// -- 예: filePath = '/WEB-INF/sql/test.json'; 
+	// 2. springboot처럼 war, jar내부의 파일을 읽어오는데 사용할 수 있습니다.
+	// 3. webRoot가 null 인 경우, 사용하면 됩니다.
+	// -- 예: springboot
+	public String getRource(String filePath) throws IOException {
+		InputStream is = context.getResourceAsStream(filePath);
+		String contents = IOUtils.toString(is, "UTF-8"); 
+		return contents;
 	}
 	
 }
